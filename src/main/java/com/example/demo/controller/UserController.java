49c3084a8d02ae4aaed736c5eef976aebd0f92e3
ArSuRa <lsi2401@gmail.com>
2024-10-21 15:45:34 +0900

    Create UserController.java

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.TodoItemDto;
+import com.example.demo.service.TodoItemService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+    @GetMapping("/user")
+    public ResponseEntity<Map<String, String>> getUser() {
+        Map<String, String> response = new HashMap<>();
+        response.put("employeeId", "12345");
+        return ResponseEntity.ok(response);
+    }
+}
